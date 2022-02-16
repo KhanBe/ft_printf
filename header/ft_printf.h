9e1da1305dfa33675e399ce6c560e27466755d53
KhanBe <wistm6980@gmail.com>
2022-02-16 22:47:22 +0900

    add prototype

--- header/ft_printf.h
+++ header/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jaewoo <jaewoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 22:42:35 by jaewoo            #+#    #+#             */
-/*   Updated: 2022/02/16 22:45:03 by jaewoo           ###   ########.fr       */
+/*   Updated: 2022/02/16 22:47:05 by jaewoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 # include "libft.h"
 # include <stdarg.h>
 
+int	ft_printf(const char *str, ...);
 #endif
