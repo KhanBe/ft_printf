2953006d80f1554283162004c168371e49f80b60
KhanBe <wistm6980@gmail.com>
2022-02-22 22:02:42 +0900

    make ft_r_s_p

--- printf/ft_reversed_string_print.c
+++ printf/ft_reversed_string_print.c
@@ -1,35 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_p.c                                             :+:      :+:    :+:   */
+/*   ft_reversed_string_print.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaewoo <jaewoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/20 19:52:12 by jaewoo            #+#    #+#             */
-/*   Updated: 2022/02/22 22:01:12 by jaewoo           ###   ########.fr       */
+/*   Created: 2022/02/22 21:57:27 by jaewoo            #+#    #+#             */
+/*   Updated: 2022/02/22 22:00:13 by jaewoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_p(unsigned long address)
+int	ft_reversed_string_print(char *str)
 {
-	int		bytes;
-	char	*number;
-	int		i;
+	int	i;
+	int	bytes;
 
-	bytes = 0;
-	if (!address)
-		return (write(1, "0x0", sizeof(char) * 3));
-	bytes = write(1, "0x", sizeof(char) * 2);
-	num = (char *)malloc(((ft_hex_length(address)) + 1) * sizeof(char));
-	while (address)
+	bytes = ft_strlen(str);
+	i = bytes - 1;
+	while (0 <= i)
 	{
-		num[i++] = ft_decimal_to_hex(address % 16, 'x');
-		address /= 16;
+		write(1, &str[i], sizeof(char) * 1);
+		i--;
 	}
-	num[i] = '\0';
-	bytes += ft_reversed_string_print(number);
-	ft_free(&number);
 	return (bytes);
 }
